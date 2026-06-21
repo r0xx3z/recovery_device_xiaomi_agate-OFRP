84c2eeda58888461eb082ef54a835e4a1cac2fb8
Veruz <axelgpg2290@gmail.com>
2026-06-20 21:51:05 -0500

    Add files via upload

---
+++ vendorsetup.sh
@@ -0,0 +1,66 @@
+# Copyright (C) 2025-2026 OrangeFox Recovery Project
+# Copyright (C) 2026 chickendrop89
+# SPDX-License-Identifier: GPL-3.0-only
+
+FDEVICE="agate"
+#set -o xtrace
+
+fox_get_target_device() {
+local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
+   if [ -n "$chkdev" ]; then 
+      FOX_BUILD_DEVICE="$FDEVICE"
+   else
+      chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
+      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+   fi
+}
+
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+   fox_get_target_device
+fi
+
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+
+# Build
+export TW_DEFAULT_LANGUAGE="en"
+export LC_ALL="C"
+export ALLOW_MISSING_DEPENDENCIES=true
+
+# Partitions
+export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
+export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
+
+export FOX_SETTINGS_ROOT_DIRECTORY="/cache"
+export FOX_MISCELLANEOUS_ROOT_DIRECTORY="/cache"
+
+# Binaries & Tools
+export FOX_USE_BUSYBOX_BINARY=1
+export FOX_USE_NANO_EDITOR=1
+export FOX_USE_TAR_BINARY=1
+export FOX_USE_SED_BINARY=1
+export FOX_USE_XZ_UTILS=1
+export FOX_USE_BASH_SHELL=1
+export FOX_ASH_IS_BASH=1
+export OF_ENABLE_FS_COMPRESSION="1"
+export FOX_ENABLE_APP_MANAGER=1
+export OF_ENABLE_FRP_ADDON="1"
+export FOX_DELETE_AROMAFM=1
+
+# Magisk & Boot Patching
+export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-v30.7.zip
+export OF_USE_MAGISKBOOT="1"
+export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES="1"
+export OF_DONT_PATCH_ENCRYPTED_DEVICE="1"
+	
+# Maintainer & Build Info
+export FOX_BUILD_TYPE="Testing"
+export FOX_MAINTAINER_PATCH_VERSION="1"
+export OF_MAINTAINER="Llorens"
+	
+	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+   	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+ 	fi
+fi
